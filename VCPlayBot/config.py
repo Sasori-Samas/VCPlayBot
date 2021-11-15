7953e5fc60570c7b9c670b8685eda82c995a5aab
Sasori-Samas <77126624+Sasori-Samas@users.noreply.github.com>
2021-11-15 18:15:27 -0500

    Update config.py

--- VCPlayBot/config.py
+++ VCPlayBot/config.py
@@ -25,6 +25,6 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", None)
 PMPERMIT = getenv("PMPERMIT", None)
 LOG_GRP = getenv("LOG_GRP", None)
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ +").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "797768146").split()))
